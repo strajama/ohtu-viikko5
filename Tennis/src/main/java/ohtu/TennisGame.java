53a4479b67d798ef4bf094893d4bf632b35c0c9e
Susanna Rajamäki <strajama@gmail.com>
2018-12-02 15:43:43 +0200

    tehtävä 2

---
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -0,0 +1,100 @@
+package ohtu;
+
+public class TennisGame {
+
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals("player1")) {
+            m_score1 += 1;
+        } else {
+            m_score2 += 1;
+        }
+    }
+
+    public String getScore() {
+        String score = "";
+//        int tempScore = 0;
+        if (this.isTie()) {
+            score = this.tie();
+        } else if (this.isAdvantageWin()) {
+            score = this.advantageWin();
+        } else {
+            score = this.gameIsOn();
+        }
+        return score;
+    }
+
+    private boolean isTie() {
+        return m_score1 == m_score2;
+    }
+
+    private String tie() {
+
+        switch (m_score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private boolean isAdvantageWin() {
+        return m_score1 >= 4 || m_score2 >= 4;
+    }
+
+    private String advantageWin() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+    private String gameIsOn() {
+        String score = "";
+        int tempScore = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = m_score1;
+            } else {
+                score += "-";
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
+    }
+}
